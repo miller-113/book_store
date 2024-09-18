06acaf7154545520125c91196c27ca6c22bd227d
Nikita <nikita.shkuratt@gmail.com>
2024-09-18 14:22:19 +0300

    delete redundant CreateApiView

--- bookstoreaggregator/books/api_urls.py
+++ bookstoreaggregator/books/api_urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api_views import BookViewSet, BookCreateApiView
+from .api_views import BookViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('books/create/', BookCreateApiView.as_view(), name='book-create'),
 ]
