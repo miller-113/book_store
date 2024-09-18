a033f7d2e85f859600797d4b96850e7b2336d2a4
Nikita <nikita.shkuratt@gmail.com>
2024-09-18 10:14:56 +0300

    completed 16

---
+++ books/templatetags/book_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from django.urls import reverse
+
+register = template.Library()
+
+@register.simple_tag
+def admin_edit(obj):
+    """
+    Generate the URL for editing an object in the admin interface.
+    """
+    return reverse(f'admin:{obj._meta.app_label}_{obj._meta.model_name}_change', args=[obj.pk])
